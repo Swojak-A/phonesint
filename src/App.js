38c21f4b5292fd55d279ad56c2c9ecacf14911f9
SwA <swojak.a@gmail.com>
2023-05-28 15:46:25 +0200

    feat: initial React setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Col>Hello World!</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
