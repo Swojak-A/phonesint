357d9687bab8af875ab4e54f22815ab506fc8946
SwA <swojak.a@gmail.com>
2023-05-28 20:42:38 +0200

    feat: add navbar element

--- src/App.js
+++ src/App.js
@@ -5,14 +5,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
-    <Container>
-      <Row>
-        <Col>Hello World!</Col>
-      </Row>
-    </Container>
+    <>
+      <Navbar bg="dark" variant="dark" className="custom-navbar">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Container>
+        <Row>
+          <Col>Hello World!</Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
