7ccd649d69eb3af28df08644dd5e8d0d68d83cba
SwA <swojak.a@gmail.com>
2023-05-29 22:54:35 +0200

    feat: update phoneNumberUtils with PL formatter

--- src/phoneNumberUtils.js
+++ src/phoneNumberUtils.js
@@ -1,15 +1,49 @@
 import { parse, isValidNumber } from 'google-libphonenumber';
 
+const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
+const PNF = require('google-libphonenumber').PhoneNumberFormat;
+
+const mobileStarts = ["45", "50", "51", "53", "57", "60", "66", "69", "72", "73", "78", "79", "88"];
+
+
+export function determinePLMobileNumber(plNumber) {
+    let isMobile = mobileStarts.some(start => plNumber.startsWith(start));
+
+    if (isMobile) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+export function formatPLPhoneNumber(phoneNumber) {
+    let nationalNumber = phoneUtil.format(phoneNumber, PNF.NATIONAL);
+    let isMobile = determinePLMobileNumber(nationalNumber);
+    
+    if (isMobile) {
+        let mobileNumber = nationalNumber.replace(/ /g, '');
+        return `+${phoneNumber.getCountryCode()} ${mobileNumber.slice(0, 3)} ${mobileNumber.slice(3, 6)} ${mobileNumber.slice(6)}`;
+    } else {
+        return phoneUtil.format(phoneNumber, PNF.INTERNATIONAL);
+    }
+}   
+
+
 export function validatePhoneNumber(number, countryCode) {
-    const phoneUtil = require('google-libphonenumber').PhoneNumberUtil.getInstance();
-    const PNF = require('google-libphonenumber').PhoneNumberFormat;
+    let formattedNumber = '';
 
     try {
         const phoneNumber = phoneUtil.parse(number, countryCode);
         if (phoneUtil.isValidNumber(phoneNumber)) {
-        return { isValid: true, message: `Good job! Your input was: ${phoneUtil.format(phoneNumber, PNF.INTERNATIONAL)}`};
+            if (countryCode === 'PL') {
+                formattedNumber = formatPLPhoneNumber(phoneNumber);
+            } else {
+                formattedNumber = phoneUtil.format(phoneNumber, PNF.INTERNATIONAL);
+            }
+            return { isValid: true, message: `Good job! Your input was: ${formattedNumber}`};
         } else {
-        return { isValid: false, message: 'This input is not a valid phone number.'};
+            return { isValid: false, message: 'This input is not a valid phone number.'};
         }
     } catch (error) {
         return { isValid: false, message: `An error occured while parsing: ${error.message}`};
