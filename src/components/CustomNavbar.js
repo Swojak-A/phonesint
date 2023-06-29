ed76e4a8b8ce078baf2dd3f328db4da6f50d9728
SwA <swojak.a@gmail.com>
2023-06-29 22:56:54 +0200

    feat: add LanguageSwitcher to CustomNavbar

--- src/components/CustomNavbar.js
+++ src/components/CustomNavbar.js
@@ -2,12 +2,21 @@ import React from "react";
 
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+import LanguageSwitcher from "./LanguageSwitcher";
 
 function CustomNavbar() {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
         <Navbar.Brand href="#home">Phonesint</Navbar.Brand>
+        <Navbar.Collapse className="justify-content-end">
+          <Nav>
+            <Navbar.Text>{"Choose your language: "}</Navbar.Text>
+            <LanguageSwitcher />
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
